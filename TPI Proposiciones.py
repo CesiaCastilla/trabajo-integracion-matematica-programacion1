5ae1b1bd7664941185b5fba963915e469ff19636
Ezequielcolque-cpu <eezequielcolque@gmail.com>
2025-09-21 19:21:46 -0300

    Delimitacion y empiezo

--- TPI Proposiciones.py
+++ TPI Proposiciones.py
@@ -1,1 +1,11 @@
-print ("hola mundo")
+#Tema logica
+import math
+#print ("hola mundo")
+#ingreso de las variables logicas (proposiciones)
+Num=int(input("Ingerese la primera proposición "))
+Num1=int(input("Ingerese la segunda proposición "))
+#Logica/operaciones
+
+#Muestra de resultados
+
+#Extencion de cantidad de variables logicas e implementacion de tablas de verdad
