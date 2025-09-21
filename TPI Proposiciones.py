50e330b3b4648d4a70ffa5301eecf4e4bd1db4fa
Ezequielcolque-cpu <eezequielcolque@gmail.com>
2025-09-21 18:31:34 -0300

    Delimitacion del archivo .py

--- TPI Proposiciones.py
+++ TPI Proposiciones.py
@@ -1,1 +1,10 @@
-print ("hola mundo")
+#Tema logica
+import math
+#print ("hola mundo")
+#ingreso de las variables logicas (proposiciones)
+
+#Logica/operaciones
+
+#Muestra de resultados
+
+#Extencion de cantidad de variables logicas e implementacion de tablas de verdad
